a979fa1d73bb3166f176037518355a4dee57cff7
Kingsosu <kingsosuolale@gmail.com>
2024-01-31 09:19:17 +0100

    second commit

--- CRMSYSTEM/settings.py
+++ CRMSYSTEM/settings.py
@@ -100,11 +100,21 @@ WSGI_APPLICATION = 'CRMSYSTEM.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default' : dj_database_url.parse(env('DATABASE_URL'))
-}
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default' : dj_database_url.parse(env('DATABASE_URL'))
+    }
 
 
+
+STATICFILES_DIRS = [ os.path.join(BASE_DIR /'static')]
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
